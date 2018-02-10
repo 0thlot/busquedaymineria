1e22f984ec216ed3b1fdd0487407b2a68d813280
oscar <oscargdlp@gmail.com>
2018-02-10 19:40:44 +0100

    Terminada LuceneBuilder sin poder comprobar

--- Practica 1/src/es/uam/eps/bmi/search/index/lucene/LuceneBuilder.java
+++ Practica 1/src/es/uam/eps/bmi/search/index/lucene/LuceneBuilder.java
@@ -14,15 +14,25 @@ import org.jsoup.Jsoup;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class LuceneBuilder implements IndexBuilder{
 
     private IndexWriter m_indexWriter;
+    private FieldType typeUrl = new FieldType();
+    private FieldType typeText  = new FieldType();
 
     public LuceneBuilder() {
-
+        typeText.setStored(true);
+        typeText.setIndexOptions(IndexOptions.DOCS_AND_FREQS);
+        typeUrl.setStored(true);
     }
 
     @Override
@@ -41,38 +51,44 @@ public class LuceneBuilder implements IndexBuilder{
         this.m_indexWriter = new IndexWriter(directory,config);
 
 
-        FieldType typeText = new FieldType();
-        typeText.setStored(true);
-        typeText.setIndexOptions(IndexOptions.DOCS_AND_FREQS);
-        FieldType typeUrl = new FieldType();
-        typeUrl.setStored(true);
-
-
         if(collectionPath.endsWith(".txt")){
+            List<String> strings = Files.readAllLines(filePath.toPath());
+            strings.parallelStream().forEach(s -> {
+                try {
+                    org.jsoup.nodes.Document d =Jsoup.connect(s).timeout(20000).get();
+                    addDocumento(d);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
 
         }else if(collectionPath.endsWith(".zip")){
+            ZipFile zipFile = new ZipFile(collectionPath);
+            List<? extends ZipEntry> files = zipFile.stream().parallel().filter((f)->{return !f.isDirectory() ;}).collect(Collectors.toList());
+            files.parallelStream().forEach(f -> {
+                InputStream is = null;
+                try {
+                    is = zipFile.getInputStream(f);
+                    org.jsoup.nodes.Document d = Jsoup.parse(is,"UTF-8",collectionPath+f.getName());
+                    if(is != null)is.close();
+                    addDocumento(d);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
 
         }else if(filePath.isDirectory()){
 
             File[] files = filePath.listFiles();
-            Arrays.stream(files).parallel().forEach((f) -> {
-
+            Arrays.stream(files).parallel().filter((f)->{return !f.isDirectory() ;}).forEach((f) -> {
                 try{
-                    Document doc = new Document();
                     org.jsoup.nodes.Document d = Jsoup.parse(f, "UTF-8");
-                    Field urlField = new Field("path", d.baseUri(), typeUrl);
-                    Field contField = new Field("texto", d.normalise().text(), typeText);
-                    doc.add(urlField);
-                    doc.add(contField);
-                    this.m_indexWriter.addDocument(doc);
-
+                    addDocumento(d);
                 }catch (IOException e){
                     throw new RuntimeException(e);
                 }
 
             });
-
-
         }else{
             throw new IOException();
         }
@@ -81,7 +97,15 @@ public class LuceneBuilder implements IndexBuilder{
 
     }
 
-    private void descomprimir(){
+    private void addDocumento(org.jsoup.nodes.Document d ) throws IOException {
 
+        if(d==null)return;
+
+        Document doc = new Document();
+        Field urlField = new Field("path", d.baseUri(), typeUrl);
+        Field contField = new Field("texto", d.normalise().text(), typeText);
+        doc.add(urlField);
+        doc.add(contField);
+        this.m_indexWriter.addDocument(doc);
     }
 }
--- Practica 1/src/es/uam/eps/bmi/search/test/TestEngine.java
+++ Practica 1/src/es/uam/eps/bmi/search/test/TestEngine.java
@@ -5,13 +5,13 @@ import es.uam.eps.bmi.search.index.Index;
 import es.uam.eps.bmi.search.index.IndexBuilder;
 import es.uam.eps.bmi.search.index.freq.FreqVector;
 import es.uam.eps.bmi.search.index.freq.TermFreq;
-import es.uam.eps.bmi.search.index.lucene.LuceneIndex;
+//import es.uam.eps.bmi.search.index.lucene.LuceneIndex;
 import es.uam.eps.bmi.search.index.lucene.LuceneBuilder;
-import es.uam.eps.bmi.search.lucene.LuceneEngine;
-import es.uam.eps.bmi.search.vsm.VSMEngine;
+//import es.uam.eps.bmi.search.lucene.LuceneEngine;
+//import es.uam.eps.bmi.search.vsm.VSMEngine;
 import es.uam.eps.bmi.search.ranking.SearchRanking;
-import es.uam.eps.bmi.search.ranking.SearchRankingDoc;
-import es.uam.eps.bmi.search.ui.TextResultDocRenderer;
+//import es.uam.eps.bmi.search.ranking.SearchRankingDoc;
+//import es.uam.eps.bmi.search.ui.TextResultDocRenderer;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -46,7 +46,7 @@ public class TestEngine {
         builder.build(collectionPath, indexPath);
         
         // Pruebas de inspección del índice
-        
+        /*
         Index index = new LuceneIndex(indexPath);
         List<String> terms = new ArrayList<String>(index.getAllTerms());
         Collections.sort(terms, new Comparator<String>() {
@@ -82,8 +82,9 @@ public class TestEngine {
 
         testSearch (new LuceneEngine(indexPath), query, 5);
         testSearch (new VSMEngine(new LuceneIndex(indexPath)), query, 5);
+        */
     }
-    
+    /*
     static void testSearch (SearchEngine engine, String query, int cutoff) throws IOException {
         System.out.println("  " + engine.getClass().getSimpleName() 
                 + ": top " + cutoff + " for query '" + query + "'");
@@ -92,7 +93,7 @@ public class TestEngine {
             System.out.println("\t" + new TextResultDocRenderer(result));
         System.out.println();
     }
-
+    */
     static void clear (String indexPath) throws IOException {
         File dir = new File(indexPath);
         if (!dir.exists()) Files.createDirectories(Paths.get(indexPath));
