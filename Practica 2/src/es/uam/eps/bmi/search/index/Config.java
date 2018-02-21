29ca5dbdeea8c4d6707fead276e842a25600dc2a
Yyandrakk <oscargdlp@gmail.com>
2018-02-21 16:19:47 +0100

    Subida practica 2

---
+++ Practica 2/src/es/uam/eps/bmi/search/index/Config.java
@@ -0,0 +1,13 @@
+package es.uam.eps.bmi.search.index;
+
+/**
+ *
+ * @author pablo
+ */
+public class Config {
+    public static String NORMS_FILE = "docnorms.dat";
+    public static String PATHS_FILE = "docpaths.dat";
+    public static String INDEX_FILE = "serialindex.dat";
+    public static String DICTIONARY_FILE = "dictionary.dat";
+    public static String POSTINGS_FILE = "postings.dat";
+}
