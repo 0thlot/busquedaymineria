f122682b9e8ec2c2cb6d330054076faee4a81606
Jorge Gomez Conde <jorge.gomezc@estudiante.uam.es>
2018-03-07 18:55:15 +0100

    Funchiona el Serializado RAM

--- Practica 2/src/es/uam/eps/bmi/search/vsm/TermBasedVSMEngine.java
+++ Practica 2/src/es/uam/eps/bmi/search/vsm/TermBasedVSMEngine.java
@@ -2,6 +2,7 @@ package es.uam.eps.bmi.search.vsm;
 import es.uam.eps.bmi.search.index.Index;
 import es.uam.eps.bmi.search.index.structure.Posting;
 import es.uam.eps.bmi.search.index.structure.PostingsList;
+import es.uam.eps.bmi.search.index.structure.PostingsListIterator;
 import es.uam.eps.bmi.search.ranking.SearchRanking;
 import es.uam.eps.bmi.search.ranking.impl.*;
 import java.io.IOException;
@@ -17,16 +18,15 @@ public class TermBasedVSMEngine extends AbstractVSMEngine{
     public SearchRanking search(String query, int cutoff) throws IOException {
         int numeroDocumentos = index.numDocs();
         RankingImpl ranking = new RankingImpl(index, cutoff);
-        PriorityQueue<RankingImplDoc> docQueue = new PriorityQueue<>(cutoff);
         HashMap<Integer, Double> hashMapDocs = new HashMap<>(); //Asocia documento y puntuacion
         String[] palabras = parse(query);
 
         for (String pa : palabras) {
             PostingsList pList = index.getPostings(pa); // Recorremos la lista de posting de cada palabra
 
-            while (pList.iterator().hasNext()) {
-                Posting post = pList.iterator().next();
-                double puntuacion = tfidf(index.getTotalFreq(pa), post.getFreq(), numeroDocumentos);
+            for(Posting post: pList) {
+                //Posting post = pList.iterator().next();
+                double puntuacion = tfidf(post.getFreq(), index.getDocFreq(pa), numeroDocumentos);
                 if(!hashMapDocs.containsKey(post.getDocID())){
                     hashMapDocs.put(post.getDocID(), (double) 0);
                 }
